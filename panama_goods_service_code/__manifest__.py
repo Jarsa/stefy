2724d400ee1f9ed9c2289f95eb55d9f665a8b97d
Hector Herrera <hector.herrera.mx@gmail.com>
2022-11-03 17:55:23 -0600

    initial commit

---
+++ panama_goods_service_code/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Panama Goods & Services Code",
+    "summary": """Panama Goods & Services Code""",
+    "description": """
+        Panama Goods & Services Code
+    """,
+    "author": "Eduweb Group",
+    "website": "https://www.eduwebgroup.com",
+    "category": "Extra Tools",
+    "version": "1.0.1",
+    "depends": [
+        "product",
+        "sale_management"
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/panama_goods_service_segment.xml",
+        "views/panama_goods_service_family.xml",
+        "views/product_product_views.xml",
+        "data/panama.goods.service.segment.csv",
+        "data/panama.goods.service.family.csv"
+    ],
+    'price': 75,
+    'currency': 'USD',
+    'license': 'OPL-1',
+}
